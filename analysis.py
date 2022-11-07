7d5e92f8945b79b03fea676980adc51fdc1e0fb0
EzequielNYC <ezequiel.esparza1998@gmail.com>
2022-11-07 12:17:09 -0500

    completed lab

--- analysis.py
+++ analysis.py
@@ -1,9 +1,21 @@
 import csv
 
-list_data = []
+list_data = [] 
 with open("states_all.csv", "r") as infile:
     # load in data as DictReader
     reader = csv.DictReader(infile)
     for row in reader:
         list_data.append(row)
 
+print(len(list_data))
+
+nys_data=[row for row in list_data if row["STATE"] == "NEW_YORK"]
+score_data = [row["AVG_READING_4_SCORE"]for row in nys_data]
+print(len(score_data))
+
+def filter(state,column):
+    nys_data =[row for row in list_data if row["STATE"] == state]
+    score_data = [row[column]for row in nys_data]
+    return score_data
+result = filter("NEW_YORK","AVG_READING_4_SCORE")
+print(len(result))
